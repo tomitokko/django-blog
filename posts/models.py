1dddeb57ff95b8539c4730f5f9642bdcd8ee6f2b
Tomi Tokko <tokkotomi09@gmail.com>
2021-04-18 11:36:50 +0100

    first commit

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
---
+++ posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'posts.html', {'posts': posts})
